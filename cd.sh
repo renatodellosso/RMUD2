4ed3218e568628cfafa2155ed4afa766daf8b349
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-04-02 12:44:36 -0400

    Fix conditional statement in cd.sh

--- cd.sh
+++ cd.sh
@@ -4,7 +4,7 @@ BRANCH=master
 cd ./../home/ubuntu/RMUD2
 git fetch
 pid=$(sudo lsof -t -i:443)
-if [[ "$(git rev-parse $BRANCH)" != "$(git rev-parse "$REMOTE/$BRANCH")" ]] || [[$pid == ""]]; then
+if [[ "$(git rev-parse $BRANCH)" != "$(git rev-parse "$REMOTE/$BRANCH")" ]] || [["$pid" == ""]]; then
     rm -rf ./cd.log
     echo "Deploying new changes at $(date): $(git log -1 --pretty=%B)"
     bash start.sh &
