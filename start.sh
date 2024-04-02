08306bd6c6bd3b0eb46f346c9a0ef40024a582a3
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-04-02 12:42:57 -0400

    Added Apache restart

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
+cd server
+dotnet build
+
 pid=$(sudo lsof -t -i:443)
 echo "Killing process $pid..."
 sudo kill $pid
 TIMEOUT_SECS=10
 timeout $TIMEOUT_SECS tail --pid=$pid -f /dev/null
 
-cd server
-dotnet build
+sudo service apache2 restart
 dotnet run
