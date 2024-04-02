28d3995ed96c4bc2b11e487a894aba511609783d
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-04-02 12:45:11 -0400

    Added log to cd

--- cd.sh
+++ cd.sh
@@ -4,6 +4,7 @@ BRANCH=master
 cd ./../home/ubuntu/RMUD2
 git fetch
 pid=$(sudo lsof -t -i:443)
+echo "Existing process: $pid"
 if [[ "$(git rev-parse $BRANCH)" != "$(git rev-parse "$REMOTE/$BRANCH")" ]] || [["$pid" == ""]]; then
     rm -rf ./cd.log
     echo "Deploying new changes at $(date): $(git log -1 --pretty=%B)"
