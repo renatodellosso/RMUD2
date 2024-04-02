790889fea867db67430aa39828c7db0c231d4361
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-04-02 12:43:48 -0400

    Removed old cd

--- cd.sh
+++ cd.sh
@@ -1,14 +1,3 @@
-echo "Pulling from git..."
-git pull
-while true
-do
-    echo "Restarting Apache..."
-    sudo service apache2 restart
-    cd server/bin/Debug/net7.0
-    echo "Starting server..."
-    sudo dotnet server.dll
-done
-
 #!/bin/bash
 REMOTE=origin
 BRANCH=master
